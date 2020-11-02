38d084a5c6772a19b370bb1f3ea756cf90b21e28
Luc Foglizzo <lucjmfoggy@gmail.com>
2020-11-02 15:42:33 -0800

    First commit, variables declared.

---
+++ script.js
@@ -0,0 +1,47 @@
+var timerEl = document.querySelector("#timer")
+var buttonEl = document.querySelector("#startButton")
+var startEl = document.querySelector("#start")
+var quizEl = document.querySelector("#quiz")
+var questionEl = document.querySelector("#question")
+var answerEl = document.querySelector("#answers")
+var doneEl = document.querySelector('#done')
+var submitButton = document.querySelector("#submit")
+var scoreEl = document.querySelector("#score")
+var backButton = document.querySelector("#back")
+var wrongEl = document.querySelector("#wrong")
+var correctEl = document.querySelector("#correct")
+var userScoreEl = document.querySelector("#userScore")
+var nameInput = document.querySelector("#name")
+var userScores = document.querySelector("#userScores")
+var clearButton = document.querySelector("#clear")
+var highScoresWl = document.querySelector("#highScores")
+
+// Declared and assigning my variables.
+
+
+//Need to start the coding quiz using a button on click.
+//document.querySelector("#start") 
+
+
+
+
+
+
+//addEventListener("click", quiz);
+
+function quiz() {
+
+};
+//Start timer and show question
+// a) question variable- b) timer variable
+
+//User is given question
+
+// User gives answer
+
+//either user is right or wrong
+//if answer is correct on to next question.
+//if answer is incorrect subtract time from timer as penalty
+
+//For game over either time=0 or all questions have been answered 
+// Once game over, be able to save both initials and score.
